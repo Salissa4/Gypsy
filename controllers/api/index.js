780be1872c3eaabceab314edff8acce5ced70708
Kosmonaut 3173 <re.huerta3173@gmail.com>
2022-11-07 20:53:03 -0600

    moved userRoutes file to api folder and filled in api/index.js file, inc

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
-const router = re
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const --
+
+router.use('/user', userRoutes);
+router.use('--', --);
+
+module.exports = router;

ee2ece4964f93add6dd3ec94dc723ccf40a96613
Monet <shelbyhernandez9876@yahoo.com>
2022-11-07 20:47:30 -0600

    implemeneted leaflet npm and created leaflet template

---
+++ public/js/leaflet.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([30.267, -97.743], 13);
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    });
+    googleStreets.addTo(map);

a42f5d4a4ae84be5139b0b4c0621bf89fb5a8d6c
Kosmonaut 3173 <re.huerta3173@gmail.com>
2022-11-07 21:32:20 -0600

    fixing eslint errors

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const { User } = require('../../models/User');
 
 router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
+  try {
+    const userData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.userId =userData.id;
-            req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.loggedIn = true;
 
-            res.status(200).json(userData);
-        });
-    } catch(err) {
-        res.status(400).json(err);
-    }
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });

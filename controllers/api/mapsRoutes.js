4a6cbdd0677fe3272781a8b42dc3025924b0b353
shawn220a <shawn.42496@gmail.com>
2022-11-10 00:04:21 -0600

    Clean up unused code

--- controllers/api/mapsRoutes.js
+++ controllers/api/mapsRoutes.js
@@ -2,17 +2,6 @@
 const router = require('express').Router();
 const { Maps } = require('../../models');
 
-//   router.get('/:id', async (req, res) => {
-//     try {
-//         const mapRendering = await Maps.findOne({});
-//         const mapMarkers = await Marker.findAll({});
-//         const mapData = [ ...mapMarkers, ...mapRendering];
-
-//       res.status(200).json(mapData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-//   });
 router.get('/', async (req, res) => {
   const allMapData = await Maps.findAll({});
   res.status(200).json(allMapData);

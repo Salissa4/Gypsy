9c45d82c33f141743d505c7bb8dc600fc69408c6
shawn220a <shawn.42496@gmail.com>
2022-11-10 00:49:59 -0600

    Put try catch around map routes

--- controllers/api/mapsRoutes.js
+++ controllers/api/mapsRoutes.js
@@ -3,25 +3,37 @@ const router = require('express').Router();
 const { Maps } = require('../../models');
 
 router.get('/', async (req, res) => {
-  const allMapData = await Maps.findAll({});
-  res.status(200).json(allMapData);
+  try {
+    const allMapData = await Maps.findAll({});
+    res.status(200).json(allMapData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:city_name', async (req, res) => {
-  const mapData = await Maps.findOne({
-    where: req.params,
-  });
-  res.status(200).json(mapData);
+  try {
+    const mapData = await Maps.findOne({
+      where: req.params,
+    });
+    res.status(200).json(mapData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
-  const newMap = await Maps.create({
-    city_name: req.body.city_name,
-    city_state: req.body.city_state,
-    map_coordinates_lat: req.body.map_coordinates_lat,
-    map_coordinates_lon: req.body.map_coordinates_lon,
-  });
-  res.status(200).json(newMap);
+  try {
+    const newMap = await Maps.create({
+      city_name: req.body.city_name,
+      city_state: req.body.city_state,
+      map_coordinates_lat: req.body.map_coordinates_lat,
+      map_coordinates_lon: req.body.map_coordinates_lon,
+    });
+    res.status(200).json(newMap);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

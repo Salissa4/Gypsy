e1afb0a97d5d1649fefef6e4a8f66ca3265525e3
shawn220a <shawn.42496@gmail.com>
2022-11-09 23:50:21 -0600

    Add get route to get all map data

--- controllers/api/mapsRoutes.js
+++ controllers/api/mapsRoutes.js
@@ -18,21 +18,26 @@ const { Marker, Maps } = require('../../models');
 //         const mapRendering = await Maps.findOne({});
 //         const mapMarkers = await Marker.findAll({});
 //         const mapData = [ ...mapMarkers, ...mapRendering];
-        
+
 //       res.status(200).json(mapData);
 //     } catch (err) {
 //         res.status(500).json(err);
 //     }
 //   });
+router.get('/', async (req, res) => {
+  const mapData = await Maps.findAll({});
+  const data = [...mapData];
+  res.status(200).json(data);
+});
 
 router.post('/', async (req, res) => {
-    const newMap = await Maps.create({
-      city_name: req.body.city_name,
-      city_state: req.body.city_state,
-      map_coordinates_lat: req.body.map_coordinates_lat,
-      map_coordinates_lon: req.body.map_coordinates_lon
-    });
-    res.json(newMap);
+  const newMap = await Maps.create({
+    city_name: req.body.city_name,
+    city_state: req.body.city_state,
+    map_coordinates_lat: req.body.map_coordinates_lat,
+    map_coordinates_lon: req.body.map_coordinates_lon,
+  });
+  res.status(200).json(newMap);
 });
 
 module.exports = router;

bd9a28d16285dce7e4cb0f54c59a7eaf78eb7e67
Monet <shelbyhernandez9876@yahoo.com>
2022-11-09 22:41:48 -0600

    configured routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,71 +1,12 @@
 const router = require("express").Router();
-const { User, Marker } = require("../models"); // TODO: Import Models
 
-router.get("/", async (req, res) => {
-  try {
-    // const Data = await Post.findAll({
-    //   attributes: ["id", "title", "content", "created_at"],
-    //   include: [
-    //    TODO: Implement Map and Marker Models
-    //   ],
-    // });
-    // const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render("homepage", { // TODO: Implement Handlebars Template
-      // posts,
-      // loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// router.get("/", async (req, res) => {
-//   try {
-//     const postData = await Post.findAll({
-//       attributes: ["id", "title", "content", "created_at"],
-//       include: [
-       // TODO: Implement Map and Marker Models
-//       ],
-//     });
-//     const posts = postData.map((post) => post.get({ plain: true }));
-
-//     res.render("", { // TODO: Implement Handlebars Template
-//       posts,
-      // loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get("/post/:id", async (req, res) => {
-//   try {
-//     const postData = await Post.findOne({
-//       where: {
-//         id: req.params.id,
-//       },
-      // TODO: Implement Map and Marker Models
-//     });
-
-//     const post = postData.get({ plain: true });
-
-//     res.render("", { // TODO: implement handlebars templates
-//       ...post,
-//       loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-router.get("/login", (req, res) => {
+router.get("/", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
 
-  res.render("login");
+  res.render("homepage");
 });
 
 router.get("/signup", (req, res) => {

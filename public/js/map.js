a57089724b3586392d7ed2cc9b7bf6013164eeab
shawn220a <shawn.42496@gmail.com>
2022-11-10 01:43:35 -0600

    Initial map is loaded

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,21 @@
-// const map = L.map('map').setView([30.267, -97.743], 5.5);
-// googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-//   maxZoom: 20,
-//   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-// });
-// googleStreets.addTo(map);
-
 const init = async () => {
   let initialMapData = await fetch('/api/maps/default');
   let { map_coordinates_lat, map_coordinates_lon } =
     await initialMapData.json();
-  console.log(map_coordinates_lat, map_coordinates_lon);
+
+  const map = L.map('map').setView(
+    [map_coordinates_lat, map_coordinates_lon],
+    5.5,
+  );
+
+  googleStreets = L.tileLayer(
+    'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+    {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    },
+  );
+  googleStreets.addTo(map);
 };
 
 // // Working function to double click to place marker on map and get coordinate data

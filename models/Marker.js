4fc0d81d2b6aa819a8488181da72a9ea75fdc5b5
shawn220a <shawn.42496@gmail.com>
2022-11-10 00:08:44 -0600

    Update model for marker to get it working minimally

--- models/Marker.js
+++ models/Marker.js
@@ -5,56 +5,56 @@ class Marker extends Model {}
 
 // TODO: Add GeoJSON data field? See Coordinates field
 Marker.init(
-{
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     // Add in note/comment from user about map marker to field?
     description: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     // Take in Leaflet Cooridnates GeoJSON into field?
     marker_coordinates_lat: {
-        type: DataTypes.GEOMETRY('POINT'),
-        allowNull: false
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
     marker_coordinates_lon: {
-        type: DataTypes.GEOMETRY('POINT'),
-        allowNull: false
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
     date_created: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id',
-        }
-    },
+    // user_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'user',
+    //     key: 'id',
+    //   },
+    // },
     map_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'maps',
-            key: 'id'
-        }
-    }
-},
-{
-     sequelize,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'maps',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'marker',
-}
+  },
 );
 
 module.exports = Marker;

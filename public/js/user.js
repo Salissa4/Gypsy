14bb7c1e8339edc84e261e00366ea8e207192836
shawn220a <shawn.42496@gmail.com>
2022-11-10 20:27:30 -0600

    Modulated handlebars more

---
+++ public/js/user.js
@@ -0,0 +1,54 @@
+const loginBtn = document.getElementById('loginbtn');
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  console.log(email, password);
+
+  // if (email && password) {
+  //   // Send a POST request to the API endpoint
+  //   const response = await fetch('/api/users/login', {
+  //     method: 'POST',
+  //     body: JSON.stringify({ email, password }),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
+
+  //   if (response.ok) {
+  //     // If successful, redirect the browser to the profile page
+  //     document.location.replace('/dashboard');
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
+};
+
+// const signupFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   const name = document.querySelector('#name-signup').value.trim();
+//   const email = document.querySelector('#email-signup').value.trim();
+//   const password = document.querySelector('#password-signup').value.trim();
+
+//   if (name && email && password) {
+//     const response = await fetch('/api/users/signup', {
+//       method: 'POST',
+//       body: JSON.stringify({ name, email, password }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/dashboard');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// };
+
+loginBtn.addEventListener('click', loginFormHandler);
+
+// const signUpBtn = document.querySelector('#signupbtn')
+//   signUpBtn.addEventListener('click', signupFormHandler);
